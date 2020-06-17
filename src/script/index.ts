aa8eff8ac2c055ba32c0fb159f247feffeee90ea
oti <otiext@gmail.com>
2020-06-17 21:27:49 +0900

    feat(script): 入力を監視して活用と読みを出力する処理を実装した

--- src/script/index.ts
+++ src/script/index.ts
@@ -2,4 +2,66 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-document.addEventListener("DOMContentLoaded", () => {});
+import { ConjugateContent } from "./ConjugateContent";
+import { ConjugateReading } from "./ConjugateReading";
+
+class Conjugator {
+  $conjugator: HTMLFormElement;
+  $content: HTMLInputElement;
+  $reading: HTMLInputElement;
+  $conjugateContent: HTMLInputElement;
+  $conjugateReading: HTMLInputElement;
+  $output: HTMLTextAreaElement;
+
+  constructor() {
+    // 要素を取得する
+    this.$conjugator = document.querySelector("#conjugator");
+    this.$content = document.querySelector("#content");
+    this.$reading = document.querySelector("#reading");
+    this.$conjugateContent = document.querySelector("#conjugateContent");
+    this.$conjugateReading = document.querySelector("#conjugateReading");
+    this.$output = document.querySelector("#output");
+
+    // 初期化
+    this.init();
+  }
+
+  get conjugatedContent() {
+    const { value: content } = this.$content;
+    const { value: seed } = this.$conjugateContent;
+
+    return seed.split(",").map((v) => v.replace("-", content));
+  }
+
+  get conjugatedReading() {
+    const { value: reading } = this.$reading;
+    const { value: seed } = this.$conjugateReading;
+
+    return seed.split(",").map((v) => v.replace("-", reading));
+  }
+
+  handleInput(ev: Event) {
+    this.$output.value =
+      this.conjugatedContent.join("\n") + this.conjugatedReading.join("\n");
+  }
+
+  dispatch() {
+    this.$conjugator.addEventListener(
+      "input",
+      (ev: Event) => {
+        this.handleInput(ev);
+      },
+      false
+    );
+  }
+
+  init() {
+    this.$conjugateContent.value = ConjugateContent;
+    this.$conjugateReading.value = ConjugateReading;
+    this.dispatch();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Conjugator();
+});
