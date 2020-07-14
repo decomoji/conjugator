8c5b40e5fc83eca766082b286165061713bc8bdf
oti <otiext@gmail.com>
2020-07-14 16:14:50 +0900

    feat(script): アクセスごとにテーマカラーがランダムに変更されるようにした

--- src/script/index.ts
+++ src/script/index.ts
@@ -2,7 +2,14 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import { Conjugator } from "./Conjugator";
+import { ThemeNames } from "./ThemeNames";
 
 document.addEventListener("DOMContentLoaded", () => {
+  // body 要素にテーミングクラスを追加する
+  document.body.classList.add(
+    ThemeNames[Math.floor(Math.random() * ThemeNames.length)]
+  );
+
+  // 活用生成を実行する
   new Conjugator();
 });
